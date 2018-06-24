4ae20dba37670f482c78455cc7c4dee2b90af1a5
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-06-24 19:03:53 +0200

    mapping request / response test

---
+++ src/test/java/MappingTest.java
@@ -0,0 +1,23 @@
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static wiremock.org.hamcrest.CoreMatchers.is;
+import static wiremock.org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by mtumilowicz on 2018-06-24.
+ */
+public class MappingTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule();
+    
+    private static TestClient testClient = new TestClient();
+
+    @Test
+    public void test() {
+        assertThat(testClient.get("/testmapping").statusCode(), is(200));
+        assertThat(testClient.get("/testmapping").content(), is("default test mapping"));
+    }
+}
