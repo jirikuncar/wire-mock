33638551c5541ec7d40830d2bdc98be7a62d4790
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-06-24 20:51:15 +0200

    removing unused fields

--- src/test/java/MappingTest.java
+++ src/test/java/MappingTest.java
@@ -1,7 +1,4 @@
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.junit.Rule;
 import org.junit.Test;
-import client.TestClient;
 
 import static wiremock.org.hamcrest.CoreMatchers.is;
 import static wiremock.org.hamcrest.MatcherAssert.assertThat;
@@ -10,12 +7,7 @@ import static wiremock.org.hamcrest.MatcherAssert.assertThat;
  * Created by mtumilowicz on 2018-06-24.
  */
 public class MappingTest extends BaseTest {
-
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule();
     
-    private static TestClient testClient = new TestClient();
-
     @Test
     public void test() {
         assertThat(testClient.get("/testmapping").statusCode(), is(200));
